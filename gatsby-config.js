e70e7cb0bc797337615f047fa0763c73109f953c
joaosilva-lp <joaosilva.lp@gmail.com>
2024-01-23 15:00:38 +0000

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,10 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
   plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
   ],
-}
+};
