904e305d99fa74ab2001de501e7090c2f90041a4
joaosilva-lp <joaosilva.lp@gmail.com>
2024-01-25 10:28:29 +0000

    just making some tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="New Post">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
